f38cff9f492b01ebe431ecff2e2b43fc99da8c3f
novicePGT <dymnam@naver.com>
2023-08-17 12:04:39 +0900

    Initial Commit

---
+++ SwiftUI_with_MVVM/SwiftUI_with_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_with_MVVMApp.swift
+//  SwiftUI_with_MVVM
+//
+//  Created by 박근태 on 2023/08/17.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_with_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
